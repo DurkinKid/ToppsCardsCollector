ba000199e8858119f568f12298e58336c97736e5
charley <charlesdurkin94@gmail.com>
2023-04-09 18:14:48 -0400

    Adding class attributes

--- main_app/views.py
+++ main_app/views.py
@@ -16,17 +16,17 @@ def topps_index(request):
     return render(request, 'topps/index.html', {'topps': topps})
 
 
-class Card:  
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
+class Topp:  
+  def __init__(self, player, team, highlight, year, company, img):
+    self.player = player
+    self.team = team
+    self.highlight = highlight
+    self.year = year
+    self.company = company
+    self.img = img
 
 topps = [
-  Card('Lolo', 'tabby', 'foul little demon', 3),
-  Card('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-  Card('Raven', 'black tripod', '3 legged cat', 4),
-  Card('Scrappy', 'french blue', 'cuddle bug', 10),
-  Card('Addie', 'golden', 'A menace', 2),
+  Topp('Mickey Mantle', 'Yankees', 'Batted .285, hit 31 home runs, and knocked in 75 ribeyes!', 1959, 'Topps', 'https://i.psacard.com/cardfacts/1959-topps-10-mickey-mantle-gem-mt-73896.jpg?h=1000' ),
+  Topp('Carl Yastrzemski', 'Red Sox', 'Batted .321, hit 14 home runs, and knocked in 68 ribeyes!', 1963, 'Topps', 'https://i.ebayimg.com/images/g/CfUAAOSwbVZkER8q/s-l500.png'),
+  Topp('Yogi Berra', 'Yankees', 'Batted .272, hit 27 home runs, and knocked in 108 ribeyes!', 1955, 'Topps', 'https://i.psacard.com/cardfacts/1955-topps-198-yogi-berra-27467.jpg?h=1000')
 ]
