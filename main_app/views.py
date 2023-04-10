9d85737254e9d4b1c0fe1e2a683819b4c775d271
charley <charlesdurkin94@gmail.com>
2023-04-09 20:04:25 -0400

    Adjusting CSS

--- main_app/views.py
+++ main_app/views.py
@@ -17,16 +17,17 @@ def topps_index(request):
 
 
 class Topp:  
-  def __init__(self, player, team, highlight, year, company, img):
+  def __init__(self, player, team, position, highlight, year, company, img):
     self.player = player
     self.team = team
+    self.position = position
     self.highlight = highlight
     self.year = year
     self.company = company
     self.img = img
 
 topps = [
-  Topp('Mickey Mantle', 'Yankees', 'Batted .285, hit 31 home runs, and knocked in 75 ribeyes!', 1959, 'Topps', 'https://i.psacard.com/cardfacts/1959-topps-10-mickey-mantle-gem-mt-73896.jpg?h=1000' ),
-  Topp('Carl Yastrzemski', 'Red Sox', 'Batted .321, hit 14 home runs, and knocked in 68 ribeyes!', 1963, 'Topps', 'https://i.ebayimg.com/images/g/CfUAAOSwbVZkER8q/s-l500.png'),
-  Topp('Yogi Berra', 'Yankees', 'Batted .272, hit 27 home runs, and knocked in 108 ribeyes!', 1955, 'Topps', 'https://i.psacard.com/cardfacts/1955-topps-198-yogi-berra-27467.jpg?h=1000')
+  Topp('Mickey Mantle', 'Yankees', 'Center Field / First Base', 'Batted .285, hit 31 home runs, and knocked in 75 ribeyes!', 1959, 'Topps', 'https://cdn5.mavin.io/production/soldItems/261479769/images/image-gallery.jpg'),
+  Topp('Carl Yastrzemski', 'Red Sox', 'Left Field', 'Batted .321, hit 14 home runs, and knocked in 68 ribeyes!', 1963, 'Topps', 'https://i.ebayimg.com/images/g/CfUAAOSwbVZkER8q/s-l500.png'),
+  Topp('Yogi Berra', 'Yankees', 'Catcher', 'Batted .272, hit 27 home runs, and knocked in 108 ribeyes!', 1955, 'Topps', 'https://i.psacard.com/cardfacts/1955-topps-198-yogi-berra-27467.jpg?h=1000')
 ]
